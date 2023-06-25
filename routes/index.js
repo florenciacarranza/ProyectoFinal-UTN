fce40d0fcef803d3f46256d66b0bfe9f828aca3e
Floremcia Carranza <florenciacarranza97@gmail.com>
2023-06-25 15:22:27 -0300

    Sitio Web

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Delicius' });
 });
 
 router.post  ('/',async(req, res, next) =>{
